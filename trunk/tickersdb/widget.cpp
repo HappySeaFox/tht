8dc1499cc8517a657eb5864a647e1a74988d0bf0
ksquirrel.iv@gmail.com <ksquirrel.iv@gmail.com@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2012-06-06 20:52:26 +0000

    Compare new tickers with old and write a timestamp

--- trunk/tickersdb/widget.cpp
+++ trunk/tickersdb/widget.cpp
@@ -17,10 +17,12 @@
 
 #include <QApplication>
 #include <QMessageBox>
+#include <QDateTime>
 #include <QSqlError>
 #include <QSqlQuery>
 #include <QTimer>
 #include <QDebug>
+#include <QFile>
 
 #include <cstdlib>
 
@@ -30,14 +32,44 @@
 #include "networkaccess.h"
 #include "csvreader.h"
 
+static const char * const TICKERS_DB = "tickers.sqlite";
+static const char * const TICKERS_DB_NEW = "tickers.sqlite.new";
+static const char * const TICKERS_DB_TS = "tickers.sqlite.timestamp";
+
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
 
+    {
+        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "old");
+        db.setDatabaseName(TICKERS_DB);
+
+        if(!db.open())
+        {
+            qDebug("Cannot open database (%s)", qPrintable(db.lastError().text()));
+            QMessageBox::critical(0, "Fatal error", QString("Cannot open database (%1)").arg(db.lastError().text()));
+            ::exit(1);
+        }
+
+        QSqlQuery query("SELECT * FROM tickers", db);
+
+        while(query.next())
+            oldTickers.append(query.value(0).toString());
+    }
+
+    QSqlDatabase::removeDatabase("old");
+
+    oldTickers.sort();
+
+    qDebug("Loaded %d old values", oldTickers.size());
+
+    QFile::remove(TICKERS_DB_NEW);
+    QFile::remove(TICKERS_DB_TS);
+
     db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("tickers.sqlite");
+    db.setDatabaseName(TICKERS_DB_NEW);
 
     if(!db.open())
     {
@@ -63,20 +95,18 @@ void Widget::slotGet()
     ui->plainTextLog->clear();
 
     QSqlQuery query;
+    int ok = 0;
 
-    QString str = "CREATE TABLE IF NOT EXISTS tickers ("
-                    "ticker VARCHAR(10),"
-                    "company VARCHAR(128),"
-                    "sector VARCHAR(128),"
-                    "industry VARCHAR(128)"
-                    ");";
+    ok += query.exec("CREATE TABLE IF NOT EXISTS tickers ("
+                         "ticker VARCHAR(10) UNIQUE,"
+                         "company VARCHAR(128),"
+                         "sector VARCHAR(128),"
+                         "industry VARCHAR(128)"
+                         ");");
 
-    bool ok = query.exec(str);
+    ok += query.exec("DELETE FROM tickers");
 
-    if(ok)
-        ok = query.exec("DELETE FROM tickers");
-
-    if(!ok)
+    if(ok != 2)
     {
         qDebug("Cannot query (%s)", qPrintable(db.lastError().text()));
         ui->plainTextLog->appendPlainText(QString("Cannot query (%1)").arg(qPrintable(db.lastError().text())));
@@ -84,10 +114,6 @@ void Widget::slotGet()
     }
 
     QSqlDatabase::database().transaction();
-
-    ui->pushGet->setEnabled(false);
-
-    qApp->processEvents();
 
     m_net->get(QUrl("http://finviz.com/export.ashx?v=150&o=ticker"));
 }
@@ -98,7 +124,6 @@ void Widget::slotFinished()
     {
         qDebug("Network error #%d", m_net->error());
         ui->plainTextLog->appendPlainText(QString("Network error #%1").arg(m_net->error()));
-        ui->pushGet->setEnabled(true);
         QSqlDatabase::database().commit();
         return;
     }
@@ -108,31 +133,70 @@ void Widget::slotFinished()
     QStringList str;
     csv.parseLine();
     int num = 0;
+    QStringList newTickers;
 
     while(!(str = csv.parseLine()).isEmpty())
     {
         if(str.size() < 5)
         {
             ui->plainTextLog->appendPlainText("Broken data");
-            ui->pushGet->setEnabled(true);
             return;
         }
 
+        newTickers.append(str[1]);
+
         if(!writeData(str[1], str[2], str[3], str[4]))
-        {
-            ui->pushGet->setEnabled(true);
             return;
-        }
 
         ui->label->setNum(++num);
 
-        if(!(num % 10))
+        if(!(num % 100))
             qApp->processEvents();
     }
 
     QSqlDatabase::database().commit();
+    QSqlDatabase::database().close();
 
-    ui->pushGet->setEnabled(true);
+    newTickers.sort();
+
+    if(newTickers != oldTickers)
+    {
+        qDebug("Need update");
+        ui->plainTextLog->appendPlainText("Need update");
+
+        if(!QFile::remove(TICKERS_DB) || !QFile::copy(TICKERS_DB_NEW, TICKERS_DB))
+        {
+            qDebug("Cannot copy");
+            ui->plainTextLog->appendPlainText("Cannot copy");
+        }
+        else
+        {
+            QFile::remove(TICKERS_DB_NEW);
+
+            QString ts = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
+            QFile fts(TICKERS_DB_TS);
+
+            if(!fts.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text))
+            {
+                qDebug("Cannot open timestamp file");
+                ui->plainTextLog->appendPlainText(QString("Cannot open timestamp file (%1)").arg(fts.errorString()));
+            }
+            else
+            {
+                fts.write(ts.toAscii());
+                fts.close();
+
+                qDebug("Update done %s", qPrintable(ts));
+                ui->plainTextLog->appendPlainText(QString("Done %1").arg(ts));
+            }
+        }
+    }
+    else
+    {
+        qDebug("Up-to-date");
+        QFile::remove(TICKERS_DB_NEW);
+        ui->plainTextLog->appendPlainText("Up-to-date");
+    }
 }
 
 bool Widget::writeData(const QString &ticker, const QString &company, const QString &sector, const QString &industry)
