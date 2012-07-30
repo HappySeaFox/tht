ec9c919965ddda9bd82caefdb69693666929794d
ksquirrel.iv@gmail.com <ksquirrel.iv@gmail.com@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2012-07-30 21:23:03 +0000

    Use http instead of https

--- trunk/ts/tht_uk.ts
+++ trunk/ts/tht_uk.ts
@@ -17,10 +17,10 @@
         <location filename="../about.ui" line="93"/>
         <source>THT helps you to do your home task in trading. It can link several Advanced Get windows together and manage a list of tickers.
 
-All suggestions and bug reports please send to our bug tracker, also visit&lt;br/&gt;&lt;a href=&quot;https://code.google.com/p/traders-home-task-ng&quot;&gt;https://code.google.com/p/traders-home-task-ng/wiki/howto&lt;/a&gt;</source>
+All suggestions and bug reports please send to our bug tracker, also visit&lt;br/&gt;&lt;a href=&quot;http://code.google.com/p/traders-home-task-ng&quot;&gt;http://code.google.com/p/traders-home-task-ng/wiki/howto&lt;/a&gt;</source>
         <translation>THT допомагає вам робити свою домашню роботу в трейдингу. Вона може лінковані кілька Advanced Get вікон разом і керувати списками тікерів.
 
-Всі побажання та звіти про помилки надсилайте на наш багтрекер, також відвідаєте &lt;br/&gt;&lt;a href=&quot;https://code.google.com/p/traders-home-task-ng/wiki/howto&quot;&gt;https://code.google.com/p/traders-home-task-ng/wiki/howto&lt;/a&gt;</translation>
+Всі побажання та звіти про помилки надсилайте на наш багтрекер, також відвідаєте &lt;br/&gt;&lt;a href=&quot;http://code.google.com/p/traders-home-task-ng/wiki/howto&quot;&gt;http://code.google.com/p/traders-home-task-ng/wiki/howto&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../about.ui" line="130"/>
