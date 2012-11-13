8564b9f54aeb9ea48c2dec93e7c2fbf1e46accf2
ksquirrel.iv@gmail.com <ksquirrel.iv@gmail.com@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2012-11-13 14:20:59 +0000

    Revert prepending ":"

--- trunk/list.cpp
+++ trunk/list.cpp
@@ -390,7 +390,7 @@ bool List::eventFilter(QObject *obj, QEvent *event)
 
                     // Google finance
                     case Qt::Key_G:
-                        openTickerInBrowser("http://www.google.com/finance?q=:%1", currentTicker(), DontFix);
+                        openTickerInBrowser("http://www.google.com/finance?q=%1", currentTicker(), DontFix);
                     break;
 
                     // Yahoo finance
