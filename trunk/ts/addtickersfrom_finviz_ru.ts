f0394a0705db06ff41eeaf7ff54af91de6bc3446
ksquirrel.iv@gmail.com <ksquirrel.iv@gmail.com@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2013-05-23 22:32:31 +0000

    CC

--- trunk/ts/addtickersfrom_finviz_ru.ts
+++ trunk/ts/addtickersfrom_finviz_ru.ts
@@ -73,12 +73,12 @@
 <context>
     <name>FinvizPlugin</name>
     <message>
-        <location filename="../finvizplugin.cpp" line="86"/>
+        <location filename="../finvizplugin.cpp" line="84"/>
         <source>Add from Finviz</source>
         <translation>Добавить из Finviz</translation>
     </message>
     <message>
-        <location filename="../finvizplugin.cpp" line="148"/>
+        <location filename="../finvizplugin.cpp" line="146"/>
         <source>Customize...</source>
         <translation>Настроить...</translation>
     </message>
