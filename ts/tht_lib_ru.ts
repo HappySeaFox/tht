e5c31f6bdf3f7ebaab028b8f396fa69faab41fc6
Dmitry Baryshev <dmitrymq@gmail.com>
2013-05-30 12:42:45 +0000

    Added line numbers

--- ts/tht_lib_ru.ts
+++ ts/tht_lib_ru.ts
@@ -4,26 +4,32 @@
 <context>
     <name>DataManagerBase</name>
     <message>
+        <location filename="../datamanagerbase.ui" line="33"/>
         <source>Name</source>
         <translation>Имя</translation>
     </message>
     <message>
+        <location filename="../datamanagerbase.ui" line="53"/>
         <source>Delete</source>
         <translation>Удалить</translation>
     </message>
     <message>
+        <location filename="../datamanagerbase.ui" line="89"/>
         <source>Clear</source>
         <translation>Очистить</translation>
     </message>
     <message>
+        <location filename="../datamanagerbase.ui" line="46"/>
         <source>Ins</source>
         <translation>Ins</translation>
     </message>
     <message>
+        <location filename="../datamanagerbase.ui" line="64"/>
         <source>Ctrl+Up</source>
         <translation>Ctrl+Up</translation>
     </message>
     <message>
+        <location filename="../datamanagerbase.ui" line="75"/>
         <source>Ctrl+Down</source>
         <translation>Ctrl+Down</translation>
     </message>
@@ -31,16 +37,19 @@
 <context>
     <name>QObject</name>
     <message>
+        <location filename="../settings.cpp" line="332"/>
         <source>Russian</source>
         <extracomment>Russian language</extracomment>
         <translation>Русский</translation>
     </message>
     <message>
+        <location filename="../settings.cpp" line="335"/>
         <source>Ukrainian</source>
         <extracomment>Ukrainian language</extracomment>
         <translation>Украинский</translation>
     </message>
     <message>
+        <location filename="../settings.cpp" line="338"/>
         <source>English</source>
         <extracomment>English language</extracomment>
         <translation>Английский</translation>
