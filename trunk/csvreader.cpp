7d268c0530785be53a6410e0262bf62583114a9e
dmitrykx <dmitrykx@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2013-06-12 06:19:42 +0000

    THT-lib has been relicensed under the terms of LGPL v3+

--- trunk/csvreader.cpp
+++ trunk/csvreader.cpp
@@ -4,6 +4,28 @@
  *  notice
  */
 
+/*
+ *  12.06.2013 update: Filonenko Mikhail allowed to license CsvParser
+ *  under the terms of LGPL v3+
+ */
+
+/*
+ * This file is part of THT.
+ *
+ * THT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * THT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with THT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "csvreader.h"
 #include <QtCore/QTextDecoder>
 
