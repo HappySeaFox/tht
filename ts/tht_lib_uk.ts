3b2b79078007e19a06e46c90070d740c0f958a6f
Dmitry Baryshev <dmitrymq@gmail.com>
2013-07-01 20:33:51 +0000

    Move inline methods from .h .to .cpp in Settings

--- ts/tht_lib_uk.ts
+++ ts/tht_lib_uk.ts
@@ -37,19 +37,19 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../settings.cpp" line="328"/>
+        <location filename="../settings.cpp" line="349"/>
         <source>Russian</source>
         <extracomment>Russian language</extracomment>
         <translation>Російська</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="331"/>
+        <location filename="../settings.cpp" line="352"/>
         <source>Ukrainian</source>
         <extracomment>Ukrainian language</extracomment>
         <translation>Українська</translation>
     </message>
     <message>
-        <location filename="../settings.cpp" line="334"/>
+        <location filename="../settings.cpp" line="355"/>
         <source>English</source>
         <extracomment>English language</extracomment>
         <translation>Англійська</translation>
