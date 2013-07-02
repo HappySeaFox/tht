512e5d615ca605d19be17412db54a3132adeb4c2
dmitrykx <dmitrykx@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2013-07-02 19:19:09 +0000

    Move the delegate to a separate class and use it in Options

--- trunk/options.cpp
+++ trunk/options.cpp
@@ -20,6 +20,7 @@
 #include <QString>
 #include <QPixmap>
 
+#include "persistentselectiondelegate.h"
 #include "settings.h"
 #include "options.h"
 
@@ -32,6 +33,7 @@ Options::Options(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->list->setItemDelegate(new PersistentSelectionDelegate(ui->list));
     ui->labelRestart->hide();
 
     QListWidgetItem *i1 = new QListWidgetItem(tr("General"), ui->list);
