4047ef4f980dd1f9853c2867576ddf99f60c8a4e
Dmitry Baryshev <dmitrymq@gmail.com>
2013-07-10 18:13:50 +0000

    Move NoEditorDelegate to a separate TU

---
+++ noeditordelegate.cpp
@@ -0,0 +1,31 @@
+/*
+ * This file is part of THT.
+ *
+ * THT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * THT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with THT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "noeditordelegate.h"
+
+NoEditorDelegate::NoEditorDelegate(QObject *parent) :
+    QStyledItemDelegate(parent)
+{}
+
+QWidget* NoEditorDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    Q_UNUSED(parent)
+    Q_UNUSED(option)
+    Q_UNUSED(index)
+
+    return 0;
+}
