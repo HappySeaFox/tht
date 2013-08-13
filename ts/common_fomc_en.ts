a97ec94ee9844385d8819cbc3378534597d2e419
Dmitry Baryshev <dmitrymq@gmail.com>
2013-08-13 11:35:24 +0000

    Added en and tr translations (en for transifex)

---
+++ ts/common_fomc_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>FomcPlugin</name>
+    <message>
+        <location filename="../fomcplugin.cpp" line="55"/>
+        <source>News from FOMC is today (click to open)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fomcplugin.cpp" line="56"/>
+        <source>FOMC</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
