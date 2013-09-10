0e50965e66def03021be3a1c486848aead3f5212
Dmitry Baryshev <dmitrymq@gmail.com>
2013-09-10 21:35:23 +0000

    Added comments on FOMC

--- ts/common_fomc_en.ts
+++ ts/common_fomc_en.ts
@@ -4,13 +4,15 @@
 <context>
     <name>FomcPlugin</name>
     <message>
-        <location filename="../fomcplugin.cpp" line="55"/>
+        <location filename="../fomcplugin.cpp" line="56"/>
         <source>News from FOMC is today (click to open)</source>
+        <extracomment>FOMC - Federal Open Market Committee. It&apos;s ok not to translate &quot;FOMC&quot; (e.g. just copy-paste &quot;FOMC&quot; to your translation)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../fomcplugin.cpp" line="56"/>
+        <location filename="../fomcplugin.cpp" line="58"/>
         <source>FOMC</source>
+        <extracomment>FOMC - Federal Open Market Committee. It&apos;s ok not to translate &quot;FOMC&quot; (e.g. just copy-paste &quot;FOMC&quot; to your translation)</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- ts/common_fomc_ru.ts
+++ ts/common_fomc_ru.ts
@@ -5,10 +5,12 @@
     <name>FomcPlugin</name>
     <message>
         <source>News from FOMC is today (click to open)</source>
+        <extracomment>FOMC - Federal Open Market Committee. It&apos;s ok not to translate &quot;FOMC&quot; (e.g. just copy-paste &quot;FOMC&quot; to your translation)</extracomment>
         <translation>Сегодня новости от FOMC (кликните чтобы открыть)</translation>
     </message>
     <message>
         <source>FOMC</source>
+        <extracomment>FOMC - Federal Open Market Committee. It&apos;s ok not to translate &quot;FOMC&quot; (e.g. just copy-paste &quot;FOMC&quot; to your translation)</extracomment>
         <translation>FOMC</translation>
     </message>
 </context>
