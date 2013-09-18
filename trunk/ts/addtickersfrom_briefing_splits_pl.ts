18d22f3f135284ff4ace34c670390e13aa03db1f
dmitrykx <dmitrykx@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2013-09-18 12:49:32 +0000

    Polish language

---
+++ trunk/ts/addtickersfrom_briefing_splits_pl.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>BriefingSplitsDownloader</name>
+    <message>
+        <location filename="../briefingsplitsdownloader.cpp" line="28"/>
+        <source>Briefing Stock Splits</source>
+        <extracomment>It&apos;s ok not to translate &quot;Briefing Stock Splits&quot; (e.g. you can just copy-paste &quot;Briefing Stock Splits&quot; to your translation). See http://www.briefing.com/investor/calendars/stock-splits</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../briefingsplitsdownloader.cpp" line="30"/>
+        <source>Downloading tickers...</source>
+        <extracomment>Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BriefingSplitsPlugin</name>
+    <message>
+        <location filename="../briefingsplitsplugin.cpp" line="40"/>
+        <source>Add from Briefing Stock Splits</source>
+        <extracomment>Command. It&apos;s ok not to translate &quot;Briefing Stock Splits&quot; (e.g. you can just copy-paste &quot;Briefing Stock Splits&quot; to your translation). See http://www.briefing.com/investor/calendars/stock-splits</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
