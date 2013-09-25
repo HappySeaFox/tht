369732264fca370b8189329557372085008c3782
Dmitry Baryshev <dmitrymq@gmail.com>
2013-09-25 11:51:11 +0000

    Added it, removed es

---
+++ ts/addtickersfrom_finviz_it.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>FinvizAccessSetup</name>
+    <message>
+        <location filename="../finvizaccesssetup.ui" line="14"/>
+        <source>Finviz access setup</source>
+        <extracomment>Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (e.g. you can just copy-paste &quot;Finviz&quot; to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizaccesssetup.ui" line="22"/>
+        <source>E-mail:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizaccesssetup.ui" line="32"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizaccesssetup.ui" line="80"/>
+        <source>If you have a FINVIZ*Elite account, you can enter your account information for access to the elite features</source>
+        <extracomment>Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (or &quot;FINVIZ*Elite&quot;) (e.g. you can just copy-paste &quot;Finviz&quot; (or &quot;FINVIZ*Elite&quot;) to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizaccesssetup.ui" line="103"/>
+        <source>Please remember that your e-mail address and password will be stored in the clear. If you don&apos;t want them stored in the clear, just leave them blank; in which case, FINVIZ*Elite options will not be accessible</source>
+        <extracomment>Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (or &quot;FINVIZ*Elite&quot;) (e.g. you can just copy-paste &quot;Finviz&quot; (or &quot;FINVIZ*Elite&quot;) to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinvizDownloader</name>
+    <message>
+        <location filename="../finvizdownloader.cpp" line="36"/>
+        <source>Finviz</source>
+        <extracomment>Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (e.g. you can just copy-paste &quot;Finviz&quot; to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizdownloader.cpp" line="38"/>
+        <source>Downloading tickers...</source>
+        <extracomment>Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to transliterate &quot;ticker&quot; to your language</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizdownloader.cpp" line="102"/>
+        <source>Broken data (fields: %1, required: %2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinvizLinkSelector</name>
+    <message>
+        <location filename="../finvizlinkselector.ui" line="14"/>
+        <source>Add from Finviz</source>
+        <extracomment>Command. Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (e.g. you can just copy-paste &quot;Finviz&quot; to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizlinkselector.ui" line="23"/>
+        <source>Add</source>
+        <extracomment>Command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinvizPlugin</name>
+    <message>
+        <location filename="../finvizplugin.cpp" line="88"/>
+        <source>Add from Finviz</source>
+        <extracomment>Command. Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (e.g. you can just copy-paste &quot;Finviz&quot; to your translation)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizplugin.cpp" line="150"/>
+        <source>Customize...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FinvizUrlManager</name>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="32"/>
+        <source>Finviz links</source>
+        <extracomment>Finviz - Stock Screener, http://finviz.com. It&apos;s ok not to translate &quot;Finviz&quot; (e.g. you can just copy-paste &quot;Finviz&quot; to your translation). Means &quot;Links to Finviz&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="35"/>
+        <source>Add</source>
+        <extracomment>Command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="40"/>
+        <source>Link</source>
+        <extracomment>Noun</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="55"/>
+        <source>Access...</source>
+        <extracomment>Noun</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="85"/>
+        <source>Tickers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../finvizurlmanager.cpp" line="85"/>
+        <location filename="../finvizurlmanager.cpp" line="99"/>
+        <source>&lt;Paste url here&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ ts/tht_lib_it.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>DataDownloader</name>
+    <message>
+        <location filename="../datadownloader.ui" line="27"/>
+        <source>Close</source>
+        <extracomment>Command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datadownloader.cpp" line="103"/>
+        <source>Network error #%1</source>
+        <extracomment>%1 will be replaced with the error code by the application. It will look like &quot;Network error #16&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DataManagerBase</name>
+    <message>
+        <location filename="../datamanagerbase.ui" line="33"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datamanagerbase.ui" line="49"/>
+        <source>Delete</source>
+        <extracomment>Command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datamanagerbase.ui" line="79"/>
+        <source>Clear</source>
+        <extracomment>Command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../tools.cpp" line="164"/>
+        <source>Open YouTube tutorial</source>
+        <extracomment>Command. Means &quot;Open a video tutorial on YouTube&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
