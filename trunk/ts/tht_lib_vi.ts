072435a3605939973d4d7e786a03097907ec787f
dmitrykx <dmitrykx@341acf0a-c2ff-4b42-b2e4-49d7c823cb34>
2013-10-09 16:22:40 +0000

    Added Vietnamese template

---
+++ trunk/ts/tht_lib_vi.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="vi_VN">
+<context>
+    <name>DataManagerBase</name>
+    <message>
+        <location filename="../datamanagerbase.ui" line="33"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datamanagerbase.ui" line="49"/>
+        <source>Delete</source>
+        <extracomment>This is the label on a button that user pushes to issue the command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../datamanagerbase.ui" line="79"/>
+        <source>Clear</source>
+        <extracomment>This is the label on a button that user pushes to issue the command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NetworkAccessDialog</name>
+    <message>
+        <location filename="../networkaccessdialog.ui" line="27"/>
+        <source>Close</source>
+        <extracomment>This is the label on a button that user pushes to issue the command</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkaccessdialog.cpp" line="152"/>
+        <source>Network error #%1</source>
+        <extracomment>%1 will be replaced with the error code by the application. It will look like &quot;Network error #16&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../tools.cpp" line="164"/>
+        <source>Open YouTube tutorial</source>
+        <extracomment>This is the label on a menu item that user clicks to issue the command. Means &quot;Open a video tutorial on YouTube&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools.cpp" line="170"/>
+        <source>Downloading tickers...</source>
+        <extracomment>Window title. Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to borrow &quot;ticker&quot; from English and incorporate into your language (see http://en.wikipedia.org/wiki/Loanword)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools.cpp" line="176"/>
+        <source>&lt;Paste url here&gt;</source>
+        <extracomment>Command addressed to the user. Means &quot;Please paste some URL here&quot;. &quot;URL&quot; can be translated as &quot;link&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../tools.cpp" line="182"/>
+        <source>Tickers</source>
+        <extracomment>Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to borrow &quot;ticker&quot; from English and incorporate into your language (see http://en.wikipedia.org/wiki/Loanword)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
