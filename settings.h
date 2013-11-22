0da5266505eeb516c68a95cddf6e9dc5f7b75f7f
Dmitry Baryshev <dmitrymq@gmail.com>
2013-11-22 18:55:17 +0000

    Define more types

--- settings.h
+++ settings.h
@@ -34,23 +34,56 @@
 Q_DECLARE_METATYPE(QList<QPoint>)
 Q_DECLARE_METATYPE(Qt::AlignmentFlag)
 
-#define SETTINGS_GET_BYTE_ARRAY Settings::instance()->value<QByteArray>
-#define SETTINGS_SET_BYTE_ARRAY Settings::instance()->setValue<QByteArray>
-
 #define SETTINGS_GET_BOOL Settings::instance()->value<bool>
 #define SETTINGS_SET_BOOL Settings::instance()->setValue<bool>
 
 #define SETTINGS_GET_INT Settings::instance()->value<int>
 #define SETTINGS_SET_INT Settings::instance()->setValue<int>
 
+#define SETTINGS_GET_UINT Settings::instance()->value<uint>
+#define SETTINGS_SET_UINT Settings::instance()->setValue<uint>
+
 #define SETTINGS_GET_LONG Settings::instance()->value<long>
 #define SETTINGS_SET_LONG Settings::instance()->setValue<long>
+
+#define SETTINGS_GET_ULONG Settings::instance()->value<unsigned long>
+#define SETTINGS_SET_ULONG Settings::instance()->setValue<unsigned long>
+
+#define SETTINGS_GET_LONG_LONG Settings::instance()->value<qlonglong>
+#define SETTINGS_SET_LONG_LONG Settings::instance()->setValue<qlonglong>
+
+#define SETTINGS_GET_ULONG_LONG Settings::instance()->value<qulonglong>
+#define SETTINGS_SET_ULONG_LONG Settings::instance()->setValue<qulonglong>
+
+#define SETTINGS_GET_FLOAT Settings::instance()->value<float>
+#define SETTINGS_SET_FLOAT Settings::instance()->setValue<float>
 
 #define SETTINGS_GET_DOUBLE Settings::instance()->value<double>
 #define SETTINGS_SET_DOUBLE Settings::instance()->setValue<double>
 
+#define SETTINGS_GET_DATE Settings::instance()->value<QDate>
+#define SETTINGS_SET_DATE Settings::instance()->setValue<QDate>
+
+#define SETTINGS_GET_TIME Settings::instance()->value<QTime>
+#define SETTINGS_SET_TIME Settings::instance()->setValue<QTime>
+
+#define SETTINGS_GET_DATE_TIME Settings::instance()->value<QDateTime>
+#define SETTINGS_SET_DATE_TIME Settings::instance()->setValue<QDateTime>
+
+#define SETTINGS_GET_BYTE_ARRAY Settings::instance()->value<QByteArray>
+#define SETTINGS_SET_BYTE_ARRAY Settings::instance()->setValue<QByteArray>
+
+#define SETTINGS_GET_BIT_ARRAY Settings::instance()->value<QBitArray>
+#define SETTINGS_SET_BIT_ARRAY Settings::instance()->setValue<QBitArray>
+
+#define SETTINGS_GET_CHAR Settings::instance()->value<QChar>
+#define SETTINGS_SET_CHAR Settings::instance()->setValue<QChar>
+
 #define SETTINGS_GET_STRING Settings::instance()->value<QString>
 #define SETTINGS_SET_STRING Settings::instance()->setValue<QString>
+
+#define SETTINGS_GET_STRING_LIST Settings::instance()->value<QStringList>
+#define SETTINGS_SET_STRING_LIST Settings::instance()->setValue<QStringList>
 
 #define SETTINGS_GET_COLOR Settings::instance()->value<QColor>
 #define SETTINGS_SET_COLOR Settings::instance()->setValue<QColor>
@@ -64,8 +97,29 @@ Q_DECLARE_METATYPE(Qt::AlignmentFlag)
 #define SETTINGS_GET_SIZE Settings::instance()->value<QSize>
 #define SETTINGS_SET_SIZE Settings::instance()->setValue<QSize>
 
+#define SETTINGS_GET_SIZEF Settings::instance()->value<QSizeF>
+#define SETTINGS_SET_SIZEF Settings::instance()->setValue<QSizeF>
+
 #define SETTINGS_GET_POINT Settings::instance()->value<QPoint>
 #define SETTINGS_SET_POINT Settings::instance()->setValue<QPoint>
+
+#define SETTINGS_GET_POINTF Settings::instance()->value<QPointF>
+#define SETTINGS_SET_POINTF Settings::instance()->setValue<QPointF>
+
+#define SETTINGS_GET_RECT Settings::instance()->value<QRect>
+#define SETTINGS_SET_RECT Settings::instance()->setValue<QRect>
+
+#define SETTINGS_GET_RECTF Settings::instance()->value<QRectF>
+#define SETTINGS_SET_RECTF Settings::instance()->setValue<QRectF>
+
+#define SETTINGS_GET_LINE Settings::instance()->value<QLine>
+#define SETTINGS_SET_LINE Settings::instance()->setValue<QLine>
+
+#define SETTINGS_GET_LINEF Settings::instance()->value<QLineF>
+#define SETTINGS_SET_LINEF Settings::instance()->setValue<QLineF>
+
+#define SETTINGS_GET_URL Settings::instance()->value<QUrl>
+#define SETTINGS_SET_URL Settings::instance()->setValue<QUrl>
 
 #define SETTINGS_REMOVE Settings::instance()->remove
 
