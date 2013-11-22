5045d33f2651d9c67abcbf63dfe2b3c016fbadca
Dmitry Baryshev <dmitrymq@gmail.com>
2013-11-22 14:44:13 +0000

    Release THT-lib under LGPL 2.1

--- tickerinformationtooltip.cpp
+++ tickerinformationtooltip.cpp
@@ -1,38 +1,38 @@
 /****************************************************************************
 **
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of the QtGui module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
-** GNU Lesser General Public License Usage
-** This file may be used under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation and
-** appearing in the file LICENSE.LGPL included in the packaging of this
-** file. Please review the following information to ensure the GNU Lesser
-** General Public License version 2.1 requirements will be met:
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
 **
-** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 **
 ** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU General
-** Public License version 3.0 as published by the Free Software Foundation
-** and appearing in the file LICENSE.GPL included in the packaging of this
-** file. Please review the following information to ensure the GNU General
-** Public License version 3.0 requirements will be met:
-** http://www.gnu.org/copyleft/gpl.html.
-**
-** Other Usage
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
-**
-**
-**
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
 **
 **
 ** $QT_END_LICENSE$
@@ -113,8 +113,8 @@ TickerInformationToolTipLabel *TickerInformationToolTipLabel::instance = 0;
 TickerInformationToolTipLabel::TickerInformationToolTipLabel(const QString &text, bool ticker, bool persist, QWidget *w)
     : QLabel(w, Qt::ToolTip | Qt::BypassGraphicsProxyWidget), styleSheetParent(0)
 {
-    delete instance;
-    instance = this;
+    delete TickerInformationToolTipLabel::instance;
+    TickerInformationToolTipLabel::instance = this;
 
     fetcher = new TickerInformationFetcher(this);
 
@@ -193,7 +193,9 @@ void TickerInformationToolTipLabel::paintEvent(QPaintEvent *ev)
 {
     QStylePainter p(this);
     QStyleOptionFrame opt;
+
     opt.init(this);
+
     p.drawPrimitive(QStyle::PE_PanelTipLabel, opt);
     p.end();
 
@@ -215,7 +217,7 @@ void TickerInformationToolTipLabel::resizeEvent(QResizeEvent *e)
 
 TickerInformationToolTipLabel::~TickerInformationToolTipLabel()
 {
-    instance = 0;
+    TickerInformationToolTipLabel::instance = 0;
     qDebug("Closing ticker info");
 }
 
@@ -272,12 +274,12 @@ bool TickerInformationToolTipLabel::eventFilter(QObject *o, QEvent *e)
     return QObject::eventFilter(o, e);
 }
 
+inline
 int TickerInformationToolTipLabel::getTipScreen(const QPoint &pos)
 {
-    if(QApplication::desktop()->isVirtualDesktop())
-        return QApplication::desktop()->screenNumber(pos);
-    else
-        return QApplication::desktop()->screenNumber();
+    return QApplication::desktop()->isVirtualDesktop()
+            ? QApplication::desktop()->screenNumber(pos)
+            : QApplication::desktop()->screenNumber();
 }
 
 void TickerInformationToolTipLabel::placeTip(const QPoint &ps)
@@ -287,6 +289,7 @@ void TickerInformationToolTipLabel::placeTip(const QPoint &ps)
     {
         //the stylesheet need to know the real parent
         TickerInformationToolTipLabel::instance->setProperty("_q_stylesheet_parent", QVariant::fromValue(0));
+
         //we force the style to be the QStyleSheetStyle, and force to clear the cache as well.
         TickerInformationToolTipLabel::instance->setStyleSheet(QLatin1String("/* */"));
 
--- ts/tht_lib_vi.ts
+++ ts/tht_lib_vi.ts
@@ -33,54 +33,54 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../tools.cpp" line="165"/>
+        <location filename="../tools.cpp" line="164"/>
         <source>Open YouTube tutorial</source>
         <extracomment>This is the label on a menu item that user clicks to issue the command. Means &quot;Open a video tutorial on YouTube&quot;</extracomment>
         <translation>Mở phần hướng dẫn bằng video trên YouTube</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="171"/>
+        <location filename="../tools.cpp" line="170"/>
         <source>Downloading tickers...</source>
         <extracomment>Window title. Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to borrow &quot;ticker&quot; from English and incorporate into your language (see http://en.wikipedia.org/wiki/Loanword)</extracomment>
         <translation>Đang tải về ticker...</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="177"/>
+        <location filename="../tools.cpp" line="176"/>
         <source>&lt;Paste url here&gt;</source>
         <extracomment>Command addressed to the user. Means &quot;Please paste some URL here&quot;. &quot;URL&quot; can be translated as &quot;link&quot;</extracomment>
         <translation>&lt;Điền vào đường dẫn liên kết tại đây&gt;</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="183"/>
+        <location filename="../tools.cpp" line="182"/>
         <source>Tickers</source>
         <extracomment>Ticker is a short company name, see http://www.investopedia.com/terms/t/tickersymbol.asp . The common practice is to borrow &quot;ticker&quot; from English and incorporate into your language (see http://en.wikipedia.org/wiki/Loanword)</extracomment>
         <translation>Tickers</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="189"/>
+        <location filename="../tools.cpp" line="188"/>
         <source>Add</source>
         <extracomment>This is the label on a button that user pushes to issue the command</extracomment>
         <translation>Thêm vào</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="194"/>
+        <location filename="../tools.cpp" line="193"/>
         <source>Error</source>
         <translation>Lỗi</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="200"/>
+        <location filename="../tools.cpp" line="199"/>
         <source>Customize</source>
         <extracomment>This is the label on a button that user pushes to issue the command</extracomment>
         <translation>Tùy chỉnh</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="206"/>
+        <location filename="../tools.cpp" line="205"/>
         <source>OK</source>
         <extracomment>This is the label on a button</extracomment>
         <translation>Đồng Ý</translation>
     </message>
     <message>
-        <location filename="../tools.cpp" line="212"/>
+        <location filename="../tools.cpp" line="211"/>
         <source>Network error #%1</source>
         <extracomment>%1 will be replaced with the error code by the application. It will look like &quot;Network error #16&quot;</extracomment>
         <translation>Lỗi mạng #%1</translation>
